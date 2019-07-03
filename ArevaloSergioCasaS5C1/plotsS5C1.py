371a61e8998763d43b676a40395035d1231b83f8
Default user <sa.arevalo@uniandes.edu.co>
2019-07-03 00:27:06 +0000

    avance del plot sin probar

---
+++ ArevaloSergioCasaS5C1/plotsS5C1.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import matplotlib.pylab as plt
+
+datos= np.genfromtxt("datosderiv.dat")
+Xcos=datos[0,:]
+Yder=datos[1,:]
+plt.figure()
+plt.plot(Yder,label="Derivada cos(x)",c="red")
+plt.plot(Xcos,label="cos(x)",c="blue")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("S5C1PLOT.pdf")
