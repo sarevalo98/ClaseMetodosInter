8b50368b679f2d774580d21fd42fe6c529e355f7
Default user <sa.arevalo@uniandes.edu.co>
2019-07-10 16:55:12 +0000

    Condiciones fijas

---
+++ Semana6/S6C3/cuerda.mk
@@ -0,0 +1,6 @@
+ArevaloSergioS6C2.png: Plots_cuerda.py datos.dat datos2.dat datos3.dat
+	python Plots_cuerda.py
+%.dat:a.out
+	./a.out
+a.out: ArevaloSergio_cuerda.cpp
+	g++ ArevaloSergio_cuerda.cpp
