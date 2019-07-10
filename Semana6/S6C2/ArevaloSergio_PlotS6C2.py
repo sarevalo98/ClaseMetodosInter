fa025f36a755ddc0ba499cad88f3be993b86c6f8
Default user <sa.arevalo@uniandes.edu.co>
2019-07-10 03:47:23 +0000

    pequeño avance

--- Semana6/S6C2/ArevaloSergio_PlotS6C2.py
+++ Semana6/S6C2/ArevaloSergio_PlotS6C2.py
@@ -2,11 +2,19 @@ import numpy as np
 import matplotlib.pylab as plt
 
 Datos= np.genfromtxt("datos.dat",delimiter=";")
-t=Datos[:,0]
-v=Datos[:,1]
+x=Datos[:,0]
+a=Datos[:,1]
+Datos2= np.genfromtxt("datos2.dat",delimiter=";")
+x2=Datos2[:,0]
+a2=Datos2[:,1]
+Datos3= np.genfromtxt("datos3.dat",delimiter=";")
+x3=Datos3[:,0]
+a3=Datos3[:,1]
 plt.figure()
-plt.plot(t,v,label="Original",c="red")
-plt.xlabel("t")
-plt.ylabel("v")
+plt.plot(x,a,label="Inicial",c="red")
+plt.plot(x2,a2,label="arrAf",c="blue")
+plt.plot(x3,a3,label="arrf",c="green")
+plt.xlabel("X")
+plt.ylabel("A")
 plt.legend()
 plt.savefig("ArevaloSergioS6C2.png")
--- Semana6/S6C2/ArevaloSergio_S6C2.cpp
+++ Semana6/S6C2/ArevaloSergio_S6C2.cpp
@@ -47,6 +47,31 @@ int main()
         outfile<<arrx[i]<<";"<<arrAi[i]<<endl;
         }
     outfile.close();
-    
+    for(int i=0;i<=N;i++)
+        {
+         arrAf[i]=(1/2)*(((c*c)*(dt*dt))/(dx*dx))*(arrAi[i+1]+arrAi[i-1]-(2*arrAi[i]))+(2*arrAi[i]);
+        }
+    ofstream outfile2;
+    outfile2.open("datos2.dat");
+    for(int i=0; i<=N;i++)
+        {
+        outfile2<<arrx[i]<<";"<<arrAf[i]<<endl;
+        }
+    outfile2.close();
+    double arrf[N];
+    for(int i=0;i<=N;i++)
+        {
+        for(int j=0;j<=N;j++)
+            {
+            arrf[j]= (2.0*(1.0-(cond*cond)))*arrAf[i] - arrAi[i] + (cond*cond)*(arrAf[i+1] +  arrAf[i-1]);
+            }
+        }
+    ofstream outfile3;
+    outfile3.open("datos3.dat");
+    for(int i=0; i<=N;i++)
+        {
+        outfile3<<arrx[i]<<";"<<arrf[i]<<endl;
+        }
+    outfile3.close();
     return 0;
     }
