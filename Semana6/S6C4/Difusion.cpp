64cbabd59db02de82e2e94ecd8e2fe90448cc3c7
Default user <sa.arevalo@uniandes.edu.co>
2019-07-12 03:14:11 +0000

    Avance S6C4

--- Semana6/S6C4/Difusion.cpp
+++ Semana6/S6C4/Difusion.cpp
@@ -10,13 +10,13 @@ int main()
     double Tc=100.0;
     double dx=0.01;
     double dy=0.01;
-    double dt=(0.5*dx)/v;
+    double dt=(0.5*dx*dx)/v;
     double L=1.0;
     int N=(L-0)/dx;
     double plac[N][N];
     for(int i=0;i<N;i++)
         {
-        for(int k=0;k<N;k++)
+        for(int k=0;k<N-1;k++)
             {
             if(i<=60&&i>=40&&k<=40&&k>=20)
                 {
@@ -39,12 +39,45 @@ int main()
         outfile<<"\n";
         }
     outfile.close();
-    double placP[N][N];
-    cout<<(v*dt)/(dx*dx)<<endl;
-    for (int i=0;i<N;i++)
-        for(int k=0;k<N;i++)
+    double placf[N][N];
+ 
+    for(double t=0.0;t<2501.0;t+=dt)
         {
-        placP[i][k]=(v*dt)/(dx*dx);
+        for (int i=1;i<N-1;i++)
+            {
+            for(int k=1;k<N-1;k++)
+                {
+                placf[i][k]=((v*dt)/(dx*dx))*(plac[i+1][k]+plac[i-1][k]-(2*plac[i][k]))+((v*dt)/(dy*dy))*(plac[i][k+1]+plac[i][k-1]-(2*plac[i][k]))+plac[i][k];
+                plac[i][k]=placf[i][k];
+                }
+            }
+        if(t==100)
+            {
+            ofstream outfile2;
+            outfile2.open("placa2.txt");
+            for(int w=0;w<N;w++)
+                {
+                for(int h=0;h<N;h++)
+                    {
+                     outfile2<<placf[w][h]<<" ";
+                     }
+                 outfile2<<"\n";
+                 }
+                 outfile2.close();
+                 }
+             if(t==2500)
+                 {ofstream outfile3;
+                  outfile3.open("placa3.txt");
+                  for(int w=0;w<N;w++)
+                      {
+                      for(int h=0;h<N;h++)
+                          {
+                           outfile3<<placf[w][h]<<" ";
+                          }
+                      outfile3<<"\n";
+                      }
+                   outfile3.close();
+                  }
         }
     return 0;
     }
--- Semana6/S6C4/Plots_Difusion.py
+++ Semana6/S6C4/Plots_Difusion.py
@@ -4,4 +4,14 @@ import matplotlib.pylab as plt
 Datos= np.loadtxt("placa.txt")
 plt.figure()
 plt.imshow(Datos)
-plt.savefig("PlacaT.png")
+plt.savefig("PlacaInicial.png")
+
+Datos2= np.loadtxt("placa2.txt")
+plt.figure()
+plt.imshow(Datos2)
+plt.savefig("Placa100.png")
+
+Datos3= np.loadtxt("placa3.txt")
+plt.figure()
+plt.imshow(Datos3)
+plt.savefig("Placa2500.png")
