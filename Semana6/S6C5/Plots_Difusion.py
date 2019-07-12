271affd5747a5f549870afaf7e6a9359a4d610dc
Default user <sa.arevalo@uniandes.edu.co>
2019-07-12 16:40:12 +0000

    primera parte hecha

--- Semana6/S6C5/Plots_Difusion.py
+++ Semana6/S6C5/Plots_Difusion.py
@@ -2,22 +2,21 @@ import numpy as np
 import matplotlib.pylab as plt
 
 Datos= np.loadtxt("placa.txt")
+Datos2= np.loadtxt("placa2.txt")
+Datos3= np.loadtxt("placa3.txt")
 plt.figure()
+plt.subplot(1,3,1)
 plt.imshow(Datos)
 plt.title("Placa inicial")
 plt.colorbar()
-plt.savefig("PlacaInicial.png")
 
-Datos2= np.loadtxt("placa2.txt")
-plt.figure()
+plt.subplot(1,3,2)
 plt.imshow(Datos2)
 plt.title("Placa en T=100")
 plt.colorbar()
-plt.savefig("Placa100.png")
 
-Datos3= np.loadtxt("placa3.txt")
-plt.figure()
+plt.subplot(1,3,3)
 plt.imshow(Datos3)
 plt.title("Placa en T=2500")
 plt.colorbar()
-plt.savefig("Placa2500.png")
+plt.savefig("PlacaFija.png")
