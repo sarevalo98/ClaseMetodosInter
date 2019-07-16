52cb377cfd504ae9543127f5a2a3059cef108068
Default user <sa.arevalo@uniandes.edu.co>
2019-07-16 22:28:07 +0000

    Avance

---
+++ Semana7/S7C1/ArevaloSergio_S7C1Random.py
@@ -0,0 +1,178 @@
+#Ejercicio 0
+#Lean el capitulo 5 del Landau (ver el programa del curso).
+import numpy as np
+import random
+import matplotlib.pylab as plt
+#Ejercicio 1
+# Usando los generadores de numeros aleatorios de numpy (https://docs.scipy.org/doc/numpy-1.15.1/reference/routines.random.html):
+# a) Genere 1000 numeros aleatorios que sigan una distribucion uniforme y esten entre -10 y 10. Haga un histograma y guardelo sin mostrarlo en un archivo llamado uniforme.pdf
+xu=np.random.uniform(-10,10,1000)
+plt.figure()
+plt.hist(xu,bins=15)
+plt.title("Distribucion uniforme")
+plt.savefig("uniforme.pdf")
+# a) Genere 1000 numeros aleatorios que sigan una distribucion gausiana centrada en 17 y de sigma 5. Haga un histograma y guardelo sin mostrarlo en un archivo llamado gausiana.pdf
+xg= np.random.normal(17, 5, 100)
+plt.figure()
+plt.hist(xg,bins=10)
+plt.title("Distribucion Gausiana")
+plt.savefig("gausiana.pdf")
+# Ejercicio 2
+# Escriba un programa en Python que: 
+# Genere puntos aleatorios distribuidos uniformemente dentro de un cuadrado de lado 30.5. Grafique sus puntos y guarde la grafica sin mostrarla en un archivo llamado cuadrado.pdf. 
+# Genere puntos aleatorios distribuidos uniformemente dentro de circulo de radio 23. Grafique sus puntos y guarde la grafica sin mostrarla en un archivo llamado circulo.pdf. 
+x=np.random.uniform(-30,30,10000)
+y=np.random.uniform(-30,30,10000)
+
+condicion=np.where(x**2+y**2<23**2)
+xcon=x[condicion]
+ycon=y[condicion]
+plt.figure()
+plt.scatter(x,y)
+plt.scatter(xcon,ycon)
+plt.title("Circulo")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("circulo.pdf")
+
+x1=np.random.uniform(-10,40.5,1000)
+y1=np.random.uniform(-10,40.5,1000)
+xcua=np.random.uniform(0,30.5,1000)
+ycua=np.random.uniform(0,30.5,1000)
+
+plt.figure()
+plt.scatter(x1,y1)
+plt.scatter(xcua,ycua)
+plt.title("Cuadrado")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("cuadrado.pdf")
+# Ejercicio 3 
+# Lean sobre caminatas aleatorias.
+
+
+# Ejercicio 4
+# Tome los puntos distribuidos aleatoriamente dentro del cuadrado y haga que cada punto siga una caminata aleatoria de 100 pasos. 
+# La magnitud de los pasos de esta caminata debe seguir una distribucion gaussiana centrada en el punto y de sigma igual a 0.25
+# Implemente condiciones de frontera periodicas: si un punto se "sale" de cuadrado por un lado, "entra" por el otro  
+def caminata1(a0,b0,s0):
+    xcua2=a0
+    ycua2=b0
+    sigma= s0
+    for i in range(100):
+        newxcua=np.random.normal(xcua2,sigma)
+        newycua=np.random.normal(ycua2,sigma)
+        xcua2=newxcua
+        ycua2=newycua
+        for k in range(len(a0)):
+            if xcua2[k]>30.5:
+                xcua2[k]= xcua2[k]-30.5
+            if ycua2[k]>30.5:
+                ycua2[k]= ycua2[k]-30.5
+            if xcua2[k]<0:
+                xcua2[k]=30.5-(abs(xcua2[k]))
+            if ycua2[k]<0:
+                ycua2[k]=30.5-(abs(ycua2[k]))
+    return xcua2,ycua2
+# Grafique la distribucion final de puntos y guarde dicha grafica sin mostrarla en un archivo llamado DistCaminata.pdf
+# Grafique la caminata de UNO de sus puntos y guarde dicha grafica sin mostrarla en un archivo llamado puntoCaminata.pdf
+plt.figure()
+plt.scatter(caminata1(xcua,ycua,0.25)[0],caminata1(xcua,ycua,0.25)[1],label="Caminados")
+plt.scatter(xcua,ycua,label="Originales")
+plt.title("Sigma=0.25")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("DistCaminata.pdf")
+
+def caminata2(a0,b0,s0):
+    xcua2=a0
+    ycua2=b0
+    arrx=[]
+    arry=[]
+    sigma= s0
+    for i in range(100):
+        newxcua=np.random.normal(xcua2,sigma)
+        newycua=np.random.normal(ycua2,sigma)
+        xcua2=newxcua
+        ycua2=newycua
+        for k in range(len(a0)):
+            if xcua2[k]>30.5:
+                xcua2[k]= xcua2[k]-30.5
+            if ycua2[k]>30.5:
+                ycua2[k]= ycua2[k]-30.5
+            if xcua2[k]<0:
+                xcua2[k]=30.5-(abs(xcua2[k]))
+            if ycua2[k]<0:
+                ycua2[k]=30.5-(abs(ycua2[k]))
+        arrx.append(xcua2[1])
+        arry.append(ycua2[1])
+    return arrx,arry
+plt.figure()
+plt.plot(caminata2(xcua,ycua,0.25)[0],caminata2(xcua,ycua,0.25)[1],label="Caminados")
+plt.title("Sigma=0.25")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("puntoCaminata.pdf")
+# Repita el proceso para sigma = 0.00025 y sigma= 2.5. Grafique la caminata de UNO de sus puntos para los distintos sigmas y guardela sin mostrarla en sigmaCaminata.pdf
+plt.figure()
+plt.subplot(1,2,1)
+plt.plot(caminata2(xcua,ycua,0.00025)[0],caminata2(xcua,ycua,0.00025)[1],label="Caminados")
+plt.title("Sigma=0.00025")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.subplot(1,2,2)
+plt.plot(caminata2(xcua,ycua,2.5)[0],caminata2(xcua,ycua,2.5)[1],label="Caminados")
+plt.title("Sigma=2.5")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("sigmaCaminata.pdf")
+# Repita el proceso para condiciones abiertas: si un punto se "sale" del cuadrado deja de ser considerado en la simulacion.
+
+# Si le queda tiempo puede:
+
+##################################################################################################################################################################
+############################################################ Ejercicio  ##########################################################################
+##################################################################################################################################################################
+
+#difusion: una gota de crema en un Cafe.
+#
+#Condiciones iniciales:
+#Cafe: 10000 particulas distribuidas uniformemente dentro de un circulo de radio igual a raiz de 230
+#Crema: 100 particulas distribuidas uniformemente dentro de un circulo de radio igual a raiz de 2
+#
+#Nota: si su codigo se esta demorando mucho en correr, puede usar 1000 particulas de cafe en vez de 10000.
+#
+# 1) Haga una grafica de las condiciones iniciales donde los dos tipos de particulas tengan distintos colores. Guarde dicha grafica sin mostrarla en CafeLecheIni.pdf
+#
+#2) Todas las particulas deben hacer una caminata aleatoria de 1000 pasos. Los pasos en las coordenadas x y deben seguir una distribucion gausiana de sigma 2.5. Si va a usar coordenadas polares elija un sigma apropiado.
+#
+#3) Condiciones de frontera: implemente unas condiciones tales que si la particulas "sale" del circulo, usted vuelva a dar el paso. Si no puede implementar solo las condiciones antes descritas, debe al menos escribir comentarios explicando que hace cada linea de codigo de las condiciones propuestas (comentado abajo)
+#
+# 4) Haga una grafica de las posiciones finales de las particulas despues de la caminata donde los dos tipos de particulas tengan distintos colores. Guarde dicha grafica sin mostrarla en CafeLecheFin.pdf
+#
+
+import numpy as np
+import matplotlib.pylab as plt
+
+
+#Una posible implementacion de condiciones de frontera. Trate de hacer la suya propia sin usar esta. 
+#Si usa esta (obtiene menos puntos) debe comentar cada una de las lineas explicando en palabras que hace el codigo. Debe tambien naturalmente usar los nombres de variables que uso en el resto de su codigo propio.
+#indexcafe=np.where((xcafenuevo*xcafenuevo+ycafenuevo*ycafenuevo)>230)
+#indexcrema=np.where((xcremanuevo*xcremanuevo+ycremanuevo*ycremanuevo)>230)
+#while(len(indexcafe[0])>1):
+#	xcafenuevo[indexcafe]=xcafe[indexcafe] + np.random.normal(0,sigma)
+#	ycafenuevo[indexcafe]=ycafe[indexcafe] + np.random.normal(0,sigma)
+#	indexcafe=np.where((xcafenuevo*xcafenuevo+ycafenuevo*ycafenuevo)>=230)
+#while(len(indexcrema[0])>1):
+#	xcremanuevo[indexcrema]=xcrema[indexcrema] + np.random.normal(0,sigma)
+#	ycremanuevo[indexcrema]=ycrema[indexcrema] + np.random.normal(0,sigma)
+#	indexcrema=np.where((xcremanuevo*xcremanuevo+ycremanuevo*ycremanuevo)>=230) 
+
+
+
+	
